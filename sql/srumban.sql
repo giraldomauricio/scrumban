0639edb41a28bcef76b5bda093b3448ca3fe3698
Mauricio Giraldo <mgiraldo@gmail.com>
2012-12-28 14:02:56 -0500

    Updated DB structure

--- sql/srumban.sql
+++ sql/srumban.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Dec 11, 2012 at 11:26 AM
+-- Generation Time: Dec 28, 2012 at 02:02 PM
 -- Server version: 5.0.37
 -- PHP Version: 4.4.7
 -- 
@@ -24,7 +24,7 @@ CREATE TABLE `main_logs` (
   `log_date` date NOT NULL,
   PRIMARY KEY  (`log_id`),
   KEY `log_task` (`log_task`,`log_user`,`log_state`,`log_date`)
-) ENGINE=MyISAM AUTO_INCREMENT=72 DEFAULT CHARSET=latin1 AUTO_INCREMENT=72 ;
+) ENGINE=MyISAM AUTO_INCREMENT=160 DEFAULT CHARSET=latin1 AUTO_INCREMENT=160 ;
 
 -- --------------------------------------------------------
 
@@ -40,7 +40,7 @@ CREATE TABLE `main_projects` (
   PRIMARY KEY  (`pro_id`),
   KEY `pro_name` (`pro_name`,`pro_status`),
   KEY `pro_team` (`pro_team`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 -- --------------------------------------------------------
 
@@ -56,7 +56,7 @@ CREATE TABLE `main_sprints` (
   `sprint_status` int(11) default NULL,
   PRIMARY KEY  (`sprint_id`),
   KEY `sprint_project` (`sprint_project`,`sprint_start`,`sprint_end`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
 -- --------------------------------------------------------
 
@@ -76,7 +76,7 @@ CREATE TABLE `main_tasks` (
   PRIMARY KEY  (`task_id`),
   KEY `task_project` (`task_project`,`task_user`,`task_state`,`task_units`,`task_title`),
   KEY `task_sprint` (`task_sprint`)
-) ENGINE=MyISAM AUTO_INCREMENT=52 DEFAULT CHARSET=latin1 AUTO_INCREMENT=52 ;
+) ENGINE=MyISAM AUTO_INCREMENT=72 DEFAULT CHARSET=latin1 AUTO_INCREMENT=72 ;
 
 -- --------------------------------------------------------
 
@@ -87,8 +87,12 @@ CREATE TABLE `main_tasks` (
 CREATE TABLE `main_teams` (
   `team_id` int(9) NOT NULL auto_increment,
   `team_name` varchar(100) NOT NULL,
+  `team_leader` int(9) NOT NULL default '1',
+  `team_notify` int(1) NOT NULL default '0',
   PRIMARY KEY  (`team_id`),
-  KEY `team_name` (`team_name`)
+  KEY `team_name` (`team_name`),
+  KEY `team_leader` (`team_leader`),
+  KEY `team_notify` (`team_notify`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
@@ -104,6 +108,7 @@ CREATE TABLE `main_users` (
   `use_key` varchar(100) NOT NULL,
   `use_name` varchar(100) NOT NULL,
   `use_team` int(9) NOT NULL default '1',
+  `use_pushover` varchar(100) NOT NULL,
   PRIMARY KEY  (`use_id`),
   KEY `use_email` (`use_email`,`use_password`,`use_key`),
   KEY `use_team` (`use_team`)
