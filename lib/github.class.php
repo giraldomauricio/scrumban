005dc38fc6a6743a798775f704637a6516cd2d61
Mauricio Giraldo <mgiraldo@gmail.com>
2012-12-31 11:56:12 -0500

    Added basic GitHub API support

---
+++ lib/github.class.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Library to support basic GitHub API commands
+ *
+ * @author mgiraldo
+ * @uses:
+ * $git = new github();
+   $git->owner = "owner";
+   $git->repo = "respository_name";
+   $git->getIssues();
+   print_r($git->obj);
+ */
+
+
+class github {
+
+    var $version = 3;
+    var $owner = "";
+    var $repo = "";
+    var $uri = "https://api.github.com/";
+    var $app = "repos/";
+    var $action = "";
+    var $obj;
+    var $raw;
+
+    public function github($owner, $repo) {
+        $this->owner = $owner;
+        $this->repo = $repo;
+    }
+
+    // Load Json result into the object
+    // Uses curl in order to avoid ssl wrapper problems
+    private function loadJson() {
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_URL, $this->uri.$this->app.$this->owner."/".$this->repo);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "/".$this->action);
+        $this->raw = curl_exec($ch);
+        $this->obj = json_decode($this->raw);
+        curl_close($ch);
+    }
+
+    public function getIssues() {
+        $this->action = "issues";
+        $this->loadJson();
+    }
+
+}
+
+?>
